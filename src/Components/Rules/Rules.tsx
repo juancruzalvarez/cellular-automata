e16106d25d7fd91c4c08061c43685db2fb55bafa
Juan Cruz Alvarez <juancruzalvarezgonzalez@gmail.com>
2022-02-13 22:26:03 -0300

    g

--- src/Components/Rules/Rules.tsx
+++ src/Components/Rules/Rules.tsx
@@ -1,18 +1,35 @@
 import './Rules.css'
 import {Ruleset} from '../../Services/rules'
+import {Dropdown, Stack} from 'react-bootstrap'
 type RulesProps = {
    boardSize: number;
    ruleset: Ruleset;
 };
 export const Rules = (props: RulesProps) =>{
    return( 
-      <div className='rulesContainer'>
+      <Stack>
          <h2>Rules</h2>
-         <h3>Board type:</h3>
+         <Stack direction='horizontal'>
+            <h3>Board:</h3>
+            <Dropdown>
+               <Dropdown.Toggle variant="success" id="dropdown-basic">
+                  Dropdown Button
+               </Dropdown.Toggle>
+
+               <Dropdown.Menu>
+                  <Dropdown.Item href="#/action-1">Torus</Dropdown.Item>
+                  <Dropdown.Item href="#/action-2">Outside Dead</Dropdown.Item>
+                  <Dropdown.Item href="#/action-3">Outside Alive</Dropdown.Item>
+               </Dropdown.Menu>
+            </Dropdown>
+         </Stack>
+         <div>
          <h3>Board size:</h3>
+         <h3>heñ</h3>
+         </div>
          <h3>Neighbours:</h3>
          <h3>Life conditions:</h3>
          <h3>Death conditions:</h3>
-      </div>
+      </Stack>
    );
 }
